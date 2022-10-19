a7620233623577c0ac4de1cdc4017a7b93731d56
Grace Pinney <pinneygrace@gmail.com>
2022-10-19 19:17:56 -0400

    all tests passing

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgres://vlzgvqic:NOddzKCe_1DTahqTntyHNhDQNVFdfP2F@peanut.db.elephantsql.com/vlzgvqic",
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -3,10 +3,10 @@ const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 const reviewsRouter = require("../reviews/reviews.router");
-//const theatersRouter = require("../theaters/theaters.router");
+const theatersRouter = require("../theaters/theaters.router");
 
 router.use("/:movieId/reviews", reviewsRouter);
-//router.use("/:movieId/theaters", theatersRouter);
+router.use("/:movieId/theaters", theatersRouter);
 
 router.route("/:movieId").get(controller.read).all(methodNotAllowed);
 router.route("/").get(controller.list).all(methodNotAllowed);
